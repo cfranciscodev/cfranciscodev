ccbe04c62adf41c2c0e2b92c3e9796f6cfda951e
cfranciscodev <cfranciscodev@gmail.com>
2014-09-26 02:21:56 +0000

    Initial Commit of project

---
+++ DataDrivenRules/DataDrivenRule_UnitTest/RuleExecutor_UnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataDrivenRules;
+using NUnit.Framework;
+
+namespace DataDrivenRule_UnitTest
+{
+    [TestFixture]
+    public class RuleExecutor_UnitTest
+    {
+        private RuleExecutor _executor;
+       
+        private void Setup()
+        {
+                _executor = new RuleExecutor();
+        }
+
+        [Test]
+        public void Monkey()
+        {
+            Setup();
+            _executor.Execute("DataDrivenRules.RuleExecutor", "MethodA");
+
+            Assert.AreEqual("MethodA", _executor.calledMethod);
+        }
+    }
+}
---
+++ DataDrivenRules/DataDrivenRules/RuleExecutor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace DataDrivenRules
+{
+    public class RuleExecutor
+    {
+        public string calledMethod = string.Empty;
+
+        public void Execute(string Class, string methodName)
+        {
+            Type objectExecutor = Type.GetType(Class);
+            ConstructorInfo magicConstructor = objectExecutor.GetConstructor(Type.EmptyTypes);
+            object magicClassObject = magicConstructor.Invoke(new object[] { });
+
+            // Get the ItsMagic method and invoke with a parameter value of 100
+
+            MethodInfo magicMethod = objectExecutor.GetMethod(methodName);
+            object magicValue = magicMethod.Invoke(magicClassObject, new object[] { });
+
+        }
+
+        public void MethodA()
+        {
+            calledMethod = "MethodA";
+        }
+
+        public void MethodB()
+        {
+            calledMethod = "MethodB";
+        }
+    }
+}
